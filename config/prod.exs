39aaa0996af81304736e2f597364cd036d2e64a9
JK Gunnink <jgunnink@gmail.com>
2016-10-12 01:16:40 +0800

    Adds config and procfile

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :noot, Noot.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "noot-w.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :noot, Noot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -64,4 +72,4 @@ config :comeonin, bcrypt_log_rounds: 14
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
