9f3856f584f4b9c1f61ece180708868d5c4ae5b2
JK Gunnink <jk@thefrontiergroup.com.au>
2016-10-11 18:39:35 +0800

    Adds migration and route

---
+++ web/models/user.ex
@@ -0,0 +1,20 @@
+defmodule Noot.User do
+  use Noot.Web, :model
+
+  schema "users" do
+    field :username, :string
+    field :email, :string
+    field :password_digest, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:username, :email, :password_digest])
+    |> validate_required([:username, :email, :password_digest])
+  end
+end
---
+++ web/views/user_view.ex
@@ -0,0 +1,3 @@
+defmodule Noot.UserView do
+  use Noot.Web, :view
+end
