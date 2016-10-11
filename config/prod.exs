9b33c3aa28c0ae89061ec0dd7c79fba8e6da15fb
JK Gunnink <jgunnink@gmail.com>
2016-10-12 01:23:30 +0800

    Fix syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :noot, Noot.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "noot-w.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
